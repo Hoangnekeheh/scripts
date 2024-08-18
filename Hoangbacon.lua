50acd8759fd2725ebce2ebbf99e906d85ace3e29
Hoangnekeheh <caothathoang84@gmail.com>
2024-08-19 00:16:12 +0700

    Update and rename blox-fruits.lua to Hoangbacon.lua

---
+++ Hoangbacon.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoang-bacon/Blox-Fruits-Script/main/blox%20fruit%20script.txt"))()
--- blox-fruits.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Error-IDK/Blox-Fruits-Script/main/blox%20fruit%20script.txt"))()
